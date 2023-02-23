c2f4f704c3cdb091c7a5d78e9d29650c75757c8a
SeoBlack <sooreennadum2001@gmail.com>
2023-02-23 15:34:02 +0200

    adding a function to refetch the items after a day from being fetche.

--- app.js
+++ app.js
@@ -6,17 +6,17 @@ import fetch from "node-fetch"
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 const covers  = {
-    "Horizon":[
+    Horizon:[
         "https://wallpaperaccess.com/full/4063068.jpg",
         "https://asset.vg247.com/horizon-forbidden-west-review-vg247.jpg/BROK/resize/1920x1920%3E/format/jpg/quality/80/horizon-forbidden-west-review-vg247.jpg",
         "https://images.hdqwalls.com/wallpapers/the-horizon-forbidden-west-4k-gq.jpg"
     ],
-    "RedDead":[
+    RedDead:[
         "https://i.pinimg.com/originals/53/d0/67/53d067277bd54f2e6999831bb20e896b.jpg",
         "https://sm.ign.com/ign_in/news/r/red-dead-2/red-dead-2s-end-credits-story-explained_ef93.jpg",
         "https://images5.alphacoders.com/917/917971.jpg"
     ],
-    "TheLastOfUs":[
+    TheLastOfUs:[
         "https://4kwallpapers.com/images/wallpapers/the-last-of-us-part-ii-ellie-playstation-4-2020-games-2880x1800-1877.jpg",
         "https://cdn.mos.cms.futurecdn.net/wA4TLMvLAohUa8wRzqodH7.jpg",
         "https://cdn.vox-cdn.com/thumbor/n_HTukfTE3gNmBmLzpLE8QSaxqk=/0x0:1200x800/1200x800/filters:focal(504x304:696x496)/cdn.vox-cdn.com/uploads/chorus_image/image/66956314/LastOfUs2_SonyInteractiveEntertainment_Ringer.0.jpg"
